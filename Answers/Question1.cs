a71c77fde9f5b13bc6db50b81c1fbdfc87c94a83
TMGMike <u1764905@hud.ac.uk>
2018-10-23 19:53:36 +0100

    Finished question. Lowered binary length to 7.

--- Answers/Question1.cs
+++ Answers/Question1.cs
@@ -10,8 +10,47 @@ namespace C_Sharp_Challenge_Skeleton.Answers
     {
         public static int Answer(int[] portfolios)
         {
-            //TODO: Please work out the solution;
-            return -1;
+            var toBase = 2;
+            var length = 7;
+            var currentBiggest = 0;
+
+            for (var portfolioIndex = 0; portfolioIndex < portfolios.Length; portfolioIndex++)
+            {
+                // Convert the element at the current index into a binary array
+                var digit = Convert.ToString(portfolios[portfolioIndex], toBase).PadLeft(length, '0');
+                var binary = digit.Select(c => c - '0').ToArray();
+
+                // Loop through the array again, to compare the current element to the others.
+                for (var i = 0; i < portfolios.Length; i++)
+                {
+                    if (i == portfolioIndex) continue; // Skip itself in the array.
+
+                    // Convert the element at i to binary array.
+                    var digit2 = Convert.ToString(portfolios[i], toBase).PadLeft(length, '0');
+                    var binary2 = digit2.Select(c => c - '0').ToArray();
+
+                    // Initialise the combined portfolio
+                    int[] combinedPortfolio = {0, 0, 0, 0, 0, 0, 0};
+                    
+                    // Loop through the two binary arrays of length 7, and populate the combined using XOR logic 
+                    for (var index = 0; index < 7; index++)
+                        if (binary[index] == 0)
+                        {
+                            if (binary2[index] == 1) combinedPortfolio[index] = 1;
+                        }
+                        else
+                        {
+                            if (binary2[index] == 0) combinedPortfolio[index] = 1;
+                        }
+                    
+                    // Convert the combined portfolio array back into a standard number
+                    var combinedInt = Convert.ToInt32(string.Join("", combinedPortfolio), 2);
+                    
+                    if (combinedInt > currentBiggest) currentBiggest = combinedInt;
+                }
+            }
+
+            return currentBiggest;
         }
     }
 }
